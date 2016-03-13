a57780975f0e31173c3485bd0f96a3389942487b
RaviTezu <ravi-teja@live.com>
2016-03-13 18:10:25 +0530

    Added bostonjuly2012.py

--- week-1/bostonjuly2012.py
+++ week-1/bostonjuly2012.py
@@ -1,9 +1,48 @@
+from __future__ import print_function
+
 import os
+import pylab
 
-DATA_FILE = "julyTemps.txt"
+# It is assumed that the 'julyTemps.txt' file is present along the side of this script and this script is
+# executed at the root.
+PWD = os.getcwd()
+FILE_NAME = 'julyTemps.txt'
+FILE = PWD + '/' + FILE_NAME
 
-def load_data(datafile=DATA_FILE):
+HIGH = []
+LOW = []
 
-    with datafile as f:
-        for line in f.readlines():
-            print(line)
+
+def load_file(inFile=FILE):
+    return open(inFile, 'r')
+
+
+def read_data(fd=load_file()):
+    for line in fd.readlines():
+        fields = line.split()
+        if len(fields) < 3 or not fields[0].isdigit():
+            pass
+        else:
+            HIGH.append(fields[1])
+            LOW.append(fields[2])
+
+
+def calculate_diff(high=HIGH, low=LOW):
+    diff_temps = [int(h) - int(l) for h,l in zip(high, low)]
+    return diff_temps
+
+
+def plotting(diff_temps):
+    length = len(diff_temps)
+    print(length)
+    pylab.figure(1)
+    pylab.title('Day by Day Ranges in Temperature in Boston in July 2012')
+    pylab.xlabel('Days')
+    pylab.ylabel('Temperature Ranges')
+    pylab.plot(range(1, length + 1), diff_temps)
+    pylab.show()
+
+
+if __name__ == "__main__":
+    read_data()
+    plotting(calculate_diff())
